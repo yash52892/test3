f5cdd19cc24d0fd1a782e6e3a5b51fdd07ebce7a
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-14 00:56:50 +0530

    Update MealItem.js

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,57 +1,104 @@
 import classes from './MealItem.module.css';
-import { useContext } from 'react';
-import CartContext from '../../../store/cart-context';
-import ListContext from '../../../store/itemContext';
 
 const MealItem = (props) => {
-  
-  const cartCtx = useContext(CartContext);
-  const list=useContext(ListContext);
 
-  const price = props.price;
+  const id=props.id;
 
-  
   const quantityLhandler=async ()=>{
-    const res=await fetch("https://crudcrud.com/api/e4df7dd43ccb4fd793ee0fe10cd0f781/cart", {
+    const l=props.sizeL;
+    const obj={
+      name: props.name,
+      description:props.description,
+      price:props.price,
+      sizeL:1,
+      sizeM:0,
+      sizeS:0
+    }
+    await fetch("https://crudcrud.com/api/a10880835f494760b5df06e34eb5ccd5/cart", {
       method: "POST",
-      body: JSON.stringify(props),
+      body: JSON.stringify(obj),
       headers: {
         "Content-Type": "application/json",
       },
      })
-     const data=await res.json();
-     console.log(data)
-
-    cartCtx.addItem(props);
-    list.sizeLquantity(props);
+     await fetch(`https://crudcrud.com/api/a10880835f494760b5df06e34eb5ccd5/products/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: props.name,
+      description:props.description,
+      price:props.price,
+      sizeL:l-1,
+      sizeM:props.sizeM,
+      sizeS:props.sizeS
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+     })
   }
   const quantityMhandler=async ()=>{
-    const res=await fetch("https://crudcrud.com/api/e4df7dd43ccb4fd793ee0fe10cd0f781/cart", {
+    const m=props.sizeM;
+    const obj={
+      name: props.name,
+      description:props.description,
+      price:props.price,
+      sizeL:0,
+      sizeM:1,
+      sizeS:0
+    }
+    await fetch("https://crudcrud.com/api/a10880835f494760b5df06e34eb5ccd5/cart", {
       method: "POST",
-      body: JSON.stringify(props),
+      body: JSON.stringify(obj),
       headers: {
         "Content-Type": "application/json",
       },
      })
-     const data=await res.json();
-     console.log(data)
-
-    cartCtx.addItem(props);
-    list.sizeLquantity(props);
+     await fetch(`https://crudcrud.com/api/a10880835f494760b5df06e34eb5ccd5/products/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: props.name,
+      description:props.description,
+      price:props.price,
+      sizeL:props.sizeL,
+      sizeM:m-1,
+      sizeS:props.sizeS
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+     })
   }
   const quantityShandler=async ()=>{
-    const res=await fetch("https://crudcrud.com/api/e4df7dd43ccb4fd793ee0fe10cd0f781/cart", {
+    const s=props.sizes;
+    const obj={
+      name: props.name,
+      description:props.description,
+      price:props.price,
+      sizeL:0,
+      sizeM:0,
+      sizeS:1
+    }
+    await fetch("https://crudcrud.com/api/a10880835f494760b5df06e34eb5ccd5/cart", {
       method: "POST",
-      body: JSON.stringify(props),
+      body: JSON.stringify(obj),
       headers: {
         "Content-Type": "application/json",
       },
      })
-     const data=await res.json();
-     console.log(data)
-
-    cartCtx.addItem(props);
-    list.sizeLquantity(props);
+     await fetch(`https://crudcrud.com/api/a10880835f494760b5df06e34eb5ccd5/products/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: props.name,
+      description:props.description,
+      price:props.price,
+      sizeL:props.sizeL,
+      sizeM:props.sizeM,
+      sizeS:s-1
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+     })
   }
 
   return (
@@ -59,7 +106,7 @@ const MealItem = (props) => {
       <div>
         <h3>{props.name}</h3>
         <span className={classes.description}>{props.description}</span>
-        <span className={classes.price}> Price: {price}</span>
+        <span className={classes.price}> Price: {props.price}</span>
       </div>
       <div>
       {props.sizeL?<button className={classes.button} onClick={quantityLhandler}>Size L:{props.sizeL}</button>:<span>Out of Stock</span>}
